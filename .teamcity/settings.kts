93fc005eba78151f254d5813d3c18a38ac9a4fd9
Amit Kumar <amikumar@guidewire.com>
2025-07-07 15:45:20 +0530

    Teamcity config with AI

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,75 +1,14 @@
-import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
-import jetbrains.buildServer.configs.kotlin.buildSteps.exec
-import jetbrains.buildServer.configs.kotlin.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.projectFeatures.githubAppConnection
-import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.*
 
-version = "2025.03"
+version = "2019.2"
 
 project {
+    description = "Generated TeamCity configuration for React project"
+    
 
-    buildType(Build)
-    buildType(CustomBuild)
-
-    features {
-        githubAppConnection {
-            id = "PROJECT_EXT_2"
-            displayName = "TeamCity-08255"
-            appId = "1504604"
-            clientId = "Iv23liKue8OVdN9WKBvP"
-            clientSecret = "credentialsJSON:91987217-92ba-42e0-939f-52b0d3dad4fe"
-            privateKey = "credentialsJSON:1cf93df6-0a46-45dc-ac63-0446b678083f"
-            ownerUrl = "https://github.com/raval08255"
-            useUniqueCallback = true
-        }
-    }
 }
 
-object Build : BuildType({
-    name = "Build"
 
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        script {
-            name = "Say hello"
-            id = "Say_hello"
-            scriptContent = """npm install"""
-        }
-    }
-
-    triggers {
-        vcs {
-        }
-    }
-
-    features {
-        perfmon {
-        }
-    }
-})
-
-object CustomBuild : BuildType({
-    name = "name"
-
-    params {
-        param("myBuildParameter", "myValue")
-    }
-
-    steps {
-        script {
-            name = "myCommandLineStep"
-            scriptContent = "npm install"
-        }
-    }
-
-    triggers {
-        vcs {
-            branchFilter = ""
-            enableQueueOptimization = false
-        }
-    }
-})
