0f81e278daea46d40049a8f5e8a19587798f8175
Amit Kumar <amikumar@guidewire.com>
2025-07-08 11:58:03 +0530

    Added teamcity build info

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,27 +1,16 @@
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServers.kotlin.buildFeatures.perfmon
-import jetbrains.buildServer.configs.kotlin.buildSteps.exec
-import jetbrains.buildServer.configs.kotlin.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.projectFeatures.githubAppConnection
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.npm
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 version = "2025.03"
 
 project {
     buildType(Build)
-    buildType(CustomBuild)
+    buildType(Lint)
 
     features {
-        githubAppConnection {
-            id = "PROJECT_EXT_2"
-            displayName = "TeamCity-08255"
-            appId = "1504604"
-            clientId = "Iv23liKue8OVdN9WKBvP"
-            clientSecret = "credentialsJSON:91987217-92ba-42e0-939f-52b0d3dad4fe"
-            privateKey = "credentialsJSON:1cf93df6-0a46-45dc-ac63-0446b678083f"
-            ownerUrl = "https://github.com/raval08255"
-            useUniqueCallback = true
-        }
+        // Add project features if needed
     }
 }
 
@@ -33,20 +22,14 @@ object Build : BuildType({
     }
 
     steps {
-        script {
+        npm {
             name = "Install Dependencies"
-            id = "Install_Dependencies"
-            scriptContent = """npm install"""
+            commands = "install"
+            executionMode = BuildStep.ExecutionMode.RUN_IF_FAILED
         }
-        script {
+        npm {
             name = "Build Project"
-            id = "Build_Project"
-            scriptContent = """npm run build"""
-        }
-        script {
-            name = "Lint Code"
-            id = "Lint_Code"
-            scriptContent = """npm run lint"""
+            commands = "run build"
         }
     }
 
@@ -61,24 +44,32 @@ object Build : BuildType({
     }
 })
 
-object CustomBuild : BuildType({
-    name = "name"
+object Lint : BuildType({
+    name = "Lint"
 
-    params {
-        param("myBuildParameter", "myValue")
+    vcs {
+        root(DslContext.settingsRoot)
     }
 
     steps {
-        script {
-            name = "myCommandLineStep"
-            scriptContent = "npm install"
+        npm {
+            name = "Install Dependencies"
+            commands = "install"
+            executionMode = BuildStep.ExecutionMode.RUN_IF_FAILED
+        }
+        npm {
+            name = "Run Lint"
+            commands = "run lint"
         }
     }
 
     triggers {
         vcs {
-            branchFilter = ""
-            enableQueueOptimization = false
+        }
+    }
+
+    features {
+        perfmon {
         }
     }
 })
