ac405bf17db943ec6797c183e3e6dcab0d9984f7
Amit Kumar <amikumar@guidewire.com>
2025-07-08 13:39:40 +0530

    Fix teamcity settings

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -34,17 +34,26 @@ object Build : BuildType({
         script {
             name = "Install Dependencies"
             id = "Install_Dependencies"
-            scriptContent = """npm install"""
+            scriptContent = """
+                export PATH=${'$'}PATH:/root/.nvm/versions/node/v22.17.0/bin
+                npm install
+            """.trimIndent()
         }
         script {
             name = "Build Project"
             id = "Build_Project"
-            scriptContent = """npm run build"""
+            scriptContent = """
+                export PATH=${'$'}PATH:/root/.nvm/versions/node/v22.17.0/bin
+                npm run build
+            """.trimIndent()
         }
         script {
             name = "Lint Code"
             id = "Lint_Code"
-            scriptContent = """npm run lint"""
+            scriptContent = """
+                export PATH=${'$'}PATH:/root/.nvm/versions/node/v22.17.0/bin
+                npm run lint
+            """.trimIndent()
         }
     }
 
